361e24fedaf93b85f51f692d56db4b6e1e8ae6f4
Lennon <lennon_lxs@hotmail.com>
2023-07-21 01:26:45 -0300

    cria a funcao  para ler arquivo txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, 'r') as file:
+            if not path_file.endswith('.txt'):
+                print('Formato inválido', file=sys.stderr)
+                return None
+
+            lines = file.read().splitlines()
+            return lines
+
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        return None
