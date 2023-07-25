b7b91d683a499298902896956795adc215be0999
Lennon <lennon_lxs@hotmail.com>
2023-07-24 21:37:05 -0300

    cria a funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
 
+def process(path_file, instance):
+    for i in range(len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return 
+        
+    lines = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(data)
+
+    print(data)
 
 def remove(instance):
     """Aqui irá sua implementação"""
