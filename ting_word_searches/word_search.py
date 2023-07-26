8483b4e82b4645057cf987eeaf8c1da3255d1392
Lennon <lennon_lxs@hotmail.com>
2023-07-26 17:17:53 -0300

    cria a funcao search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for item in instance.queue:
+        searched_words = []
+        nome_item = item['nome_do_arquivo']
+        linhas_item = item['linhas_do_arquivo']
+
+        for num_linha, linha in enumerate(linhas_item, start=1):
+            if word.lower() in linha.lower():
+                searched_words.append({
+                    "linha": num_linha,
+                    "conteudo": linha
+                    })
+
+        if searched_words:
+            result.append({
+                "palavra": word,
+                "arquivo": nome_item,
+                "ocorrencias": searched_words
+            })
+    return result
